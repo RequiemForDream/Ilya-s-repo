3499b67cb7e52cbbee3eedcbab13166cb15919fc
Eugen <122142783+RequiemForDream@users.noreply.github.com>
2024-06-12 18:09:45 +0300

    Work in progress

---
+++ Assets/Scripts/Core/AudioController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class AudioController : MonoBehaviour
+    {
+        [SerializeField] private float _timeBetweenSounds = 10f;
+
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource _doorAudio;
+        [SerializeField] private AudioSource _weatherSound;
+
+        [Header("AudioClip")]
+        [SerializeField] private AudioClip _doorSound;
+        [SerializeField] private AudioClip _weatherAudio;
+
+        private float _currentTime = 0;
+
+        private void Start()
+        {
+            _currentTime = _timeBetweenSounds;
+        }
+
+        private void Update()
+        {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime < 0 )
+            {
+
+            }
+        }
+    }
+}
