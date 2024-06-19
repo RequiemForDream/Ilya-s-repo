3dda8c6557e5de81797634ba5bc0a6d09cfd47cb
Eugen <122142783+RequiemForDream@users.noreply.github.com>
2024-06-19 21:44:07 +0300

    Ready

---
+++ Assets/Scripts/Interactables/Crane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Interactables
+{
+    public class Crane : Interactable
+    {
+        [SerializeField] private GameObject _waterPrefab;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private AudioSource _craneSource;
+
+        public override void Interact()
+        {
+            base.Interact();
+            if (_isInteracted)
+            {
+                StartCoroutine(WaterRoutine());
+                _craneSource.Play();
+            }
+            else
+            {
+                StopCoroutine(WaterRoutine());
+                _craneSource.Stop();
+            }
+        }
+
+        private IEnumerator WaterRoutine()
+        {
+            while (_isInteracted)
+            {
+                var water = Instantiate(_waterPrefab, _spawnPoint.transform.position, Quaternion.identity);
+                Destroy(water, 0.4f);
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/Pult.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Interactables
+{
+    public class Pult : Interactable
+    {
+        [SerializeField] private AudioSource _tvSource;
+        [SerializeField] private AudioSource _pultBtn;
+        [SerializeField] private AudioClip _clickClip;
+
+        [Header("Materials")]
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private Material _turnOffMaterial;
+        [SerializeField] private Material _turnOnMaterial;
+
+        private bool _isTurnedOn = false;
+
+        public override void Interact()
+        {
+           _isTurnedOn = !_isTurnedOn;
+
+            _pultBtn.PlayOneShot(_clickClip);
+            if (_isTurnedOn)
+            {
+                _tvSource.Play();
+                _meshRenderer.material = _turnOnMaterial;
+            }
+            else
+            {
+                _meshRenderer.material = _turnOffMaterial;
+                _tvSource.Stop();
+            }
+        }
+    }
+}
